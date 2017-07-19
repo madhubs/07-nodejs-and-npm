34a46a5693fbea65fd1757332ebc7012d711c377
Madeline Stevens <stevensm1987@gmail.com>
2017-07-19 10:10:48 -0700

    all TODOs completed, stretch goal completed, able to type in /new in the address bar to navigate to the new article page

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,18 +1,24 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE: Include all of the static  resources as an argument to app.use()
+let express = require('express');
+let app = express();
+
+app.use(express.static('./public'))
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+app.get('/new', function(req, res){
+  res.sendFile('public/new.html', { root: '.'});
+});
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -23,5 +29,6 @@ app.post('/articles', bodyParser, function(request, response) {
 })
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log(`Listening on port:" ${PORT}"`)
 });
